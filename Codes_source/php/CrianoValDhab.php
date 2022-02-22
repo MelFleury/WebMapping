81108b71f738030a0a807a12ded9870863412f06
@mel_fleury <melodie.fleury@yahoo.fr>
2022-02-22 22:31:56 +0100

    Update CrianoValDhab.php

--- Codes_source/php/CrianoValDhab.php
+++ Codes_source/php/CrianoValDhab.php
@@ -1,10 +1,10 @@
 <?php
 include_once ('ConnectDatabaseCheese.php');
 //REQUETE CRIANO : "Val d'Abondance"
-$CrianoValDhab = 'SELECT a."wkb_geometry" FROM "AOC_HauteSavoie_geom" as a WHERE a."AireDeProduction" = "Val_Dabondance_(Haute-Savoie)";';
+$CrianoValDhab = "SELECT wkb_geometry FROM AOC_HauteSavoie_geom WHERE AireDeProduction = 'Val_Dabondance'";
 $result = $connexion -> query($CrianoValDhab);
   while ($ligne = $result -> fetch()){
-    echo "<option value='".$ligne['wkb_geometry']."'>".$ligne['wkb_geometry']."</option>";
+    echo $ligne;
     }
 $result -> closeCursor();
 ?>
